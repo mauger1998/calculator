4e58de9e5b43736f2f13ca421505feeab8b1cc3a
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-01 16:35:33 +0100

    Create subtract function and multiply function

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const add = (firstNumberToAdd, secondNumberToAdd) => {
     return firstNumberToAdd + secondNumberToAdd;
 }
 
+const subtract = (firstNumberToSubtract, secondNumberToSubtract) => {
+    return firstNumberToSubtract - secondNumberToSubtract;
+}
+
+const multiply = (firstNumberToTimes, secondNumberToTimes) => {
+    return firstNumberToTimes * secondNumberToTimes;
+
+}
+
+console.log(multiply(10,10))
